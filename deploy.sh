540245f2826d0072f37a825ec9cd5a54ca482d51
gxkai <1378026744@qq.com>
2020-07-21 07:59:58 +0800

    docs

--- deploy.sh
+++ deploy.sh
@@ -10,21 +10,13 @@ npm run buildDocs
 
 npm run build
 
-git add -A
-
-git commit -m 'deploy'
-
-git push origin master
-
-git subtree push --prefix dist origin gh-pages
 # 进入生成的文件夹
-#cd dist
-#
-#git init
-#git add -A
-#git commit -m 'deploy'
-#
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
 ## 如果发布到 https://<USERNAME>.github.io/<REPO>
-#git push -f git@github.com:gxkai/Vue-UI-for-PC.git master:gh-pages
+git push -f git@github.com:gxkai/Vue-UI-for-PC.git master:gh-pages
 
 cd -
